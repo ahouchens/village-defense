f91b5272845dca04096d8b01dc741db72bc0d952
Alex Houchens <webtechalex@gmail.com>
2019-01-11 12:35:16 -0500

    Add build

---
+++ build/precache-manifest.5b810f3cf781d58754911ca057e951a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "feb4c9fae587236dbf51",
+    "url": "/static/css/main.48336970.chunk.css"
+  },
+  {
+    "revision": "feb4c9fae587236dbf51",
+    "url": "/static/js/main.feb4c9fa.chunk.js"
+  },
+  {
+    "revision": "f370b753e5901661dd5d",
+    "url": "/static/js/1.f370b753.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9d7c829d769eacfb69c72cc1b9f41151",
+    "url": "/index.html"
+  }
+];
