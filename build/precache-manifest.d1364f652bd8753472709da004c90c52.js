a522e7db239d39d3f38df7ef5a786a86f65a3b98
Alex Houchens <webtechalex@gmail.com>
2019-01-29 15:16:06 -0500

    Build

---
+++ build/precache-manifest.d1364f652bd8753472709da004c90c52.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8adf13e127d6d76d37aa",
+    "url": "/static/css/main.7526db23.chunk.css"
+  },
+  {
+    "revision": "8adf13e127d6d76d37aa",
+    "url": "/static/js/main.8adf13e1.chunk.js"
+  },
+  {
+    "revision": "832cb1516a89ca41c490",
+    "url": "/static/js/1.832cb151.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dc2e3afbdbc5d919dd1f27bbd2e9cab7",
+    "url": "/static/media/grass-dirt-tile-sheet.dc2e3afb.png"
+  },
+  {
+    "revision": "b6156924906868a3ec4dd419b62ed58a",
+    "url": "/static/media/player-character-2.b6156924.png"
+  },
+  {
+    "revision": "8eac3d76df44584506ede2946182cdb7",
+    "url": "/static/media/player-character.8eac3d76.png"
+  },
+  {
+    "revision": "885eb3c03c442af6e2fae75e14d0a6c8",
+    "url": "/index.html"
+  }
+];
